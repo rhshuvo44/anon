db7af4084ddd70b1025cad39dcc7f3c5ebf6a073
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-03-04 11:29:00 +0600

    feat: banner section

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,48 @@ const toastCloseBtn = document.querySelector("[data-toast-close]");
 toastCloseBtn.addEventListener("click", () => {
   notificationToast.classList.add("closed");
 });
+
+// mobile menu variables
+const mobileMenuOpenBtn = document.querySelectorAll(
+  "[data-mobile-menu-open-btn]"
+);
+const mobileMenu = document.querySelectorAll("[data-mobile-menu]");
+const mobileMenuCloseBtn = document.querySelectorAll(
+  "[data-mobile-menu-close-btn]"
+);
+const overlay = document.querySelectorAll("[data-overlay]");
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+  // mobile menu fuction
+  const mobileMenuCloseFunc = () => {
+    mobileMenu[i].classList.remove("active");
+    overlay.classList.remove("active");
+  };
+  mobileMenuOpenBtn[i].addEventListener("click", () => {
+    mobileMenu[i].classList.add("active");
+    overlay.classList.add("active");
+  });
+  mobileMenuCloseBtn[i].addEventListener("click", mobileMenuCloseFunc);
+  overlay.addEventListener("click", mobileMenuCloseFunc);
+}
+
+// accordion variables
+const accordionBtn = document.querySelectorAll("[data-accordion-btn]");
+const accordion = document.querySelectorAll("[data-accordion]");
+// console.log(accordionBtn);
+
+for (let i = 0; i < accordionBtn.length; i++) {
+  console.log("inside");
+  accordionBtn[i].addEventListener("click", () => {
+    const clickedBtn = this.nextElementSibling.classList.contains("active");
+    for (let i = 0; i < accordion.length; i++) {
+      if (clickedBtn) break;
+      if (accordion[i].classList.contains("active")) {
+        accordion[i].classList.remove("active");
+        accordionBtn[i].classList.remove("active");
+      }
+    }
+    this.nextElementSibling.classList.toggle("active");
+    this.classList.toggle("active");
+  });
+}
